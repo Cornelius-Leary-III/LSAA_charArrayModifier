7cf6654b8b87d9b527fe27373fad8730465515fe
Cornelius-Leary-III <celeary3@vt.edu>
2019-05-06 15:02:26 -0400

    Wrote C and Cpp functions; added appropriate unit tests.

--- CharacterArrayConversionFunction.h
+++ CharacterArrayConversionFunction.h
@@ -3,11 +3,12 @@
 //
 
 #include <cctype>
+#include <vector>
 
-char* convertCharacterArrayData(char* inputCharArray, int numberOfInputChars)
+using std::vector;
+
+char* convertCharacterArrayDataCStyle(char* inputCharArray, int numberOfInputChars)
 {
-    int currentCharIndex = 0;
-    
     // check if input character array is empty.
     if (inputCharArray == nullptr)
     {
@@ -20,22 +21,100 @@ char* convertCharacterArrayData(char* inputCharArray, int numberOfInputChars)
         return inputCharArray;
     }
     
-    // for each character in the array...
-    while (currentCharIndex != numberOfInputChars)
+    // create an output array with enough room for '\0' terminator-char.
+    char outputCharArray[numberOfInputChars + 1];
+    
+    // copy the input contents to the output array.
+    strcpy(outputCharArray, inputCharArray);
+    
+    int currentCharIndex = 0;
+    
+    // for each character in the array (except the null terminator)...
+    while (currentCharIndex < numberOfInputChars - 1)
     {
         if (!isalnum(inputCharArray[currentCharIndex]))
         {
             // character is non-alphanumeric.
-            inputCharArray[currentCharIndex] = '.';
+            outputCharArray[currentCharIndex] = '.';
         }
         else if (islower(inputCharArray[currentCharIndex]))
         {
             // character is lowercase.
-            inputCharArray[currentCharIndex] = toupper(inputCharArray[currentCharIndex]);
+            outputCharArray[currentCharIndex] = toupper(inputCharArray[currentCharIndex]);
         }
+        
+        std::cout << "current character: ___" << outputCharArray[currentCharIndex]
+                  << "___" << std::endl;
         
         ++currentCharIndex;
     }
     
+    std::cout << "input array:  ___" << inputCharArray << "___" << std::endl;
+    std::cout << "output array: ___" << outputCharArray << "___" << std::endl;
+    
+    inputCharArray = outputCharArray;
+    
+    std::cout << std::endl;
+    std::cout << "input array:  ___" << inputCharArray << "___" << std::endl;
+    std::cout << "output array: ___" << outputCharArray << "___" << std::endl;
+    
+    //    return outputCharArray;
+    
+    return inputCharArray;
+}
+
+char* convertCharacterArrayDataCppStyle(char* inputCharArray, int numberOfInputChars)
+{
+    // check if input character array is empty.
+    if (inputCharArray == nullptr)
+    {
+        return inputCharArray;
+    }
+    
+    // check if the input character count is a positive integer.
+    if (numberOfInputChars <= 0)
+    {
+        return inputCharArray;
+    }
+ 
+    // add each character into the vector.
+    vector<char> outputChars;
+    
+    int charIndex = 0;
+    while (charIndex < numberOfInputChars)
+    {
+        // determine current character type.
+        if (!isalnum(inputCharArray[charIndex]))
+        {
+            // character is non-alphanumeric.
+            outputChars.push_back('.');
+        }
+        else if (islower(inputCharArray[charIndex]))
+        {
+            // character is lowercase.
+            outputChars.push_back(toupper(inputCharArray[charIndex]));
+        }
+        else
+        {
+            // character is already an UPPERCASE alpha character or numeric character.
+            outputChars.push_back(inputCharArray[charIndex]);
+        }
+        
+        ++charIndex;
+    }
+    
+    char outputCharArray[outputChars.size() + 1];
+    
+    int outputCharIndex = 0;
+    int outputCharsSize = outputChars.size();
+    
+    // create output character array.
+    while (outputCharIndex < outputCharsSize)
+    {
+        outputCharArray[outputCharIndex] = outputChars[outputCharIndex];
+        ++outputCharIndex;
+    }
+    
+    inputCharArray = outputCharArray;
     return inputCharArray;
 }
--- unit_test_character_array_conversion.cpp
+++ unit_test_character_array_conversion.cpp
@@ -5,7 +5,125 @@
 #include <gtest/gtest.h>
 #include "CharacterArrayConversionFunction.h"
 
-TEST(test_input_char_array, empty_array)
+TEST(test_cstyle_input_char_array, empty_array)
 {
-
+    char* emptyCharArray = nullptr;
+    int testNumberOfChars = 5;
+    
+    char* outputCharArray = convertCharacterArrayDataCStyle(emptyCharArray, testNumberOfChars);
+    
+    EXPECT_EQ(outputCharArray, nullptr);
 }
+
+TEST(test_cstyle_number_of_input_chars, negative_count)
+{
+    char* testCharArray = "test phrase";
+    int testNumberOfChars = -9;
+    
+    char* outputCharArray = convertCharacterArrayDataCStyle(testCharArray, testNumberOfChars);
+    
+    EXPECT_EQ(outputCharArray, testCharArray);
+}
+
+TEST(test_cstyle_number_of_input_chars, zero_count)
+{
+    char* testCharArray = "another test phrase";
+    int testNumberOfChars = 0;
+    
+    char* outputCharArray = convertCharacterArrayDataCStyle(testCharArray, testNumberOfChars);
+    
+    EXPECT_EQ(outputCharArray, testCharArray);
+}
+
+TEST(test_cstyle_number_of_input_chars, positive_count)
+{
+    char testCharArray[] = "third test___phrase";
+    int testNumberOfChars = 20;
+    
+    char* outputCharArray = convertCharacterArrayDataCStyle(testCharArray, testNumberOfChars);
+    
+    std::cout << "test input array:  ___" << testCharArray << "___" << std::endl;
+    std::cout << "test output array: ___" << outputCharArray << "___" << std::endl;
+    
+    EXPECT_EQ(outputCharArray, "THIRD.TEST...PHRASE");
+}
+
+TEST(test_cpp_style_input_char_array, empty_array)
+{
+    char* emptyCharArray = nullptr;
+    int testNumberOfChars = 5;
+    
+    char* outputCharArray = convertCharacterArrayDataCppStyle(emptyCharArray, testNumberOfChars);
+    
+    EXPECT_EQ(outputCharArray, nullptr);
+}
+
+TEST(test_cpp_style_number_of_input_chars, negative_count)
+{
+    char* testCharArray = "test phrase";
+    int testNumberOfChars = -9;
+    
+    char* outputCharArray = convertCharacterArrayDataCppStyle(testCharArray, testNumberOfChars);
+    
+    EXPECT_EQ(outputCharArray, testCharArray);
+}
+
+TEST(test_cpp_style_number_of_input_chars, zero_count)
+{
+    char* testCharArray = "another test phrase";
+    int testNumberOfChars = 0;
+    
+    char* outputCharArray = convertCharacterArrayDataCppStyle(testCharArray, testNumberOfChars);
+    
+    EXPECT_EQ(outputCharArray, testCharArray);
+}
+
+TEST(test_cpp_style_number_of_input_chars, positive_count)
+{
+    char testCharArray[] = "third test___phrase";
+    int testNumberOfChars = 20;
+    
+    char* outputCharArray = convertCharacterArrayDataCppStyle(testCharArray, testNumberOfChars);
+    
+    std::cout << "test input array:  ___" << testCharArray << "___" << std::endl;
+    std::cout << "test output array: ___" << outputCharArray << "___" << std::endl;
+    
+    EXPECT_EQ(outputCharArray, "THIRD.TEST...PHRASE");
+}
+
+TEST(test_is_alnum, alpha_char_lowercase)
+{
+    char testA = 'a';
+    
+    EXPECT_TRUE(isalnum(testA));
+}
+
+TEST(test_is_alnum, alpha_char_uppercase)
+{
+    char test = 'Z';
+    
+    EXPECT_TRUE(isalnum(test));
+}
+
+TEST(test_is_alnum, numeric_char)
+{
+    char test = '9';
+    
+    EXPECT_TRUE(isalnum(test));
+}
+
+TEST(test_is_alnum, non_alpha_non_numeric_chars)
+{
+    char test = '_';
+    EXPECT_FALSE(isalnum(test));
+    
+    test = '/';
+    EXPECT_FALSE(isalnum(test));
+    
+    test = '.';
+    EXPECT_FALSE(isalnum(test));
+    
+    test = ' ';
+    EXPECT_FALSE(isalnum(test));
+}
+
